8062c59cf76c403920f916fb27aa15cea05ce46b
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2025-03-31 16:13:50 +0530

    Production

--- backendV1/src/index.ts
+++ backendV1/src/index.ts
@@ -544,9 +544,8 @@ app.get("/api/v1/:shareLink",async (req:Request,res:Response):Promise<any>=>{
 
 
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(3000,()=>{
-    console.log('server running on port 3000 !');
-})
 
 
